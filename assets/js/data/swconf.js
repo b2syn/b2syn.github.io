ad3232f519675c37f7789ee48cb01fe0d39b1800
b2syn <b2syn@users.noreply.github.com>
2025-08-08 17:11:18 +0000

    deploy: 2ed66911e4fd35672b4c96b30a2ae49e7e4986c6

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754672326',resources: [
+    cacheName: 'chirpy-1754673077',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
